f140b85d59fd89058a50047f364431efaa6bec94
Multitravel.com <ignacio.peralta@multitravel.com>
2023-06-08 11:09:20 -0300

    components

---
+++ components-lading/src/Components/Redes/index.jsx
@@ -0,0 +1,16 @@
+import 
+
+
+const Redes = () =>{
+    return(
+        <div className="main__container__redes">
+            <div className="main__container__redes__text">
+                <img src=""></img>
+                <h2>¡SEGUINOS EN NUESTRAS REDES SOCIALES!</h2>
+                <p>DESCUBRÍ ANTES QUE NADIE OFERTAS EXCLUSIVAS</p>
+            </div>
+        </div>
+    )
+}
+
+export default Redes
