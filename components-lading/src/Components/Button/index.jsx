a88ed004bdd878dc30f322d1bf1b80dcacbba4cc
Multitravel.com <ignacio.peralta@multitravel.com>
2023-07-18 11:19:35 -0300

    update

--- components-lading/src/Components/Button/index.jsx
+++ components-lading/src/Components/Button/index.jsx
@@ -1,12 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./style.css";
 
 function ButtonTresSecciones(props) {
   const [buttonText, setButtonText] = useState(props.text);
+  const clickInProgressRef = useRef(false);
 
-  const handleClick = (event) => {
+  const handleClickTresSecciones = (event) => {
     event.preventDefault();
-    window.open(props.link, '_blank');
+    
+    if (!clickInProgressRef.current) {
+      clickInProgressRef.current = true;
+      window.open(props.link, '_blank');
+    }
   };
 
   const updateButtonText = () => {
@@ -26,8 +31,19 @@ function ButtonTresSecciones(props) {
     };
   }, []);
 
+  useEffect(() => {
+    const resetClickInProgress = () => {
+      clickInProgressRef.current = false;
+    };
+
+    window.addEventListener("beforeunload", resetClickInProgress);
+    return () => {
+      window.removeEventListener("beforeunload", resetClickInProgress);
+    };
+  }, []);
+
   return (
-    <button id={props.id} className={props.style} onClick={handleClick}>
+    <button id={props.id} className={props.style} onClick={handleClickTresSecciones}>
       {buttonText}
     </button>
   );
