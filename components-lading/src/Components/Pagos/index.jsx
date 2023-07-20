54ff8970887d8ef66a0619402eb90d627ca4cf7f
Multitravel.com <ignacio.peralta@multitravel.com>
2023-07-20 17:50:52 -0300

    update

--- components-lading/src/Components/Pagos/index.jsx
+++ components-lading/src/Components/Pagos/index.jsx
@@ -6,7 +6,7 @@ function Pagos() {
     <div className="main__container_pagos_boxes">
       <Boxs
         text_h2_pagos="Vos decidís"
-        text_p_pagos="Encontrá todos los medios de pagos exclusivos para vos"
+        text_p_pagos="Encontrá todos los medios de pagos exclusivos para vos."
         position_img="boxs_right"
         btn_position="btnStylePagos__left"
         boxs_pagos_img="https://multitravelcom.github.io/components-ladings/img/MedisodePago-Desktop.png"
@@ -15,7 +15,7 @@ function Pagos() {
       />
       <Boxs
         text_h2_pagos="Quienes somos"
-        text_p_pagos="Conoce nuestro equipo y nuestra pasión"
+        text_p_pagos="Conoce nuestro equipo y nuestra pasión."
         position_img="boxs_left"
         btn_position="btnStylePagos__right"
         boxs_pagos_img="https://multitravelcom.github.io/components-ladings/img/MedisodePago-Desktop2.png"
